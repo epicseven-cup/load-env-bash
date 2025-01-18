cf7960fe65d1b37ffe14def4630dc407841fa63e
epicseven-cup <jackylin616@gmail.com>
2025-01-18 01:13:07 -0500

    basic layout finish

---
+++ README.md
@@ -0,0 +1,9 @@
+## Install
+```bash
+go install github.com/epicseven-cup/load-env-bash@latest
+```
+
+## Usage
+```bash
+load-env-bash <filepath-env>
+```
--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"runtime"
 	"strings"
 )
 
-func loadEnv(path string) error {
-	file, err := os.Open(path)
+var inputFileName string
+var outFileName string
+
+func init() {
+	const (
+		defaultInputFilePath  = "./.env"
+		defaultOutputFilePath = "./output.sh"
+	)
+	flag.StringVar(&inputFileName, "i", defaultInputFilePath, "Path to the input file (shorthand)")
+	flag.StringVar(&inputFileName, "input", defaultInputFilePath, "Path to the input file")
+
+	flag.StringVar(&outFileName, "o", defaultOutputFilePath, "Path to the output file (shorthand)")
+	flag.StringVar(&outFileName, "output", defaultOutputFilePath, "Path to the output file")
+	flag.Parse()
+}
+
+func loadEnv() error {
+	file, err := os.OpenFile(inputFileName, os.O_RDONLY, 0600)
 	if err != nil {
 		return err
 	}
+	outputFile, err := os.OpenFile(outFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(outputFile)
 	defer file.Close()
+	defer outputFile.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -21,30 +45,37 @@ func loadEnv(path string) error {
 		if len(splitLine) != 2 {
 			return fmt.Errorf("invalid env line: %s, expecting x=y", line)
 		}
-		err := os.Setenv(splitLine[0], splitLine[1])
+		_, err := writer.WriteString(fmt.Sprintf("export %s=%s\n", splitLine[0], splitLine[1]))
 		if err != nil {
 			return err
 		}
+		if err := scanner.Err(); err != nil {
+			return err
+		}
 	}
-	return scanner.Err()
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Load env succeeded")
+	return nil
 }
 
 func main() {
-	args := os.Args[1:] // Starting from 1 ignoring the binary name
-	filePath := args[0]
-	switch os := runtime.GOOS; os {
+	switch goos := runtime.GOOS; goos {
 	case "darwin":
-		fmt.Println("darwin")
+		fmt.Println("OS: darwin")
 		fmt.Println("Not supported yet")
 	case "linux":
-		fmt.Println("linux")
-		err := loadEnv(filePath)
+		fmt.Println("OS: linux")
+		err := loadEnv()
 		if err != nil {
 			fmt.Println("Error loading env:", err)
 		}
 	case "windows":
-		fmt.Println("windows")
-		err := loadEnv(filePath)
+		fmt.Println("OS: windows")
+		err := loadEnv()
 		if err != nil {
 			fmt.Println("Error loading env:", err)
 		}
